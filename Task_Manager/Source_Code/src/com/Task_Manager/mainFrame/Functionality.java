3dcd7a6b7553b25a238d23bc69ac2afb27245184
aeloori revanth kumar <71598984+aeloori@users.noreply.github.com>
2023-08-13 23:39:55 +0530

    Functionality 1.2
    added functions to update and delete task button

--- Task_Manager/Source_Code/src/com/Task_Manager/mainFrame/Functionality.java
+++ Task_Manager/Source_Code/src/com/Task_Manager/mainFrame/Functionality.java
@@ -16,31 +16,86 @@ public class Functionality extends MainFrame{
 	static LinkedList<String> ringtones;
 	static LinkedList<Integer> priorities;
 	static int index=0;
+	static int selected;
+	static String tempTaskName;
+	static String tempTaskDesc;
+	static String tempStartDate;
+	static String tempEndDate;
+	static String tempRingtone;
+	
+	static int temps;
 	
 	public static void main()
 	{
 		initiateList();
 		btnAddTask();
 		listFunction();
+//		delFunction();
+//		updateFunction();
 	}
 	
+	private static void updateFunction() {
+		updateTask.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(tempTaskName!=taskNameInput.getText() || tempTaskName!=taskDescBox.getText()||tempStartDate!=startDateInput.getText()||tempEndDate!=endDateInput.getText()||tempRingtone!=ringtoneInput.getText())
+				{
+					updateValues();
+					temps=selected;
+					System.out.println("\n updated data is ");
+					System.out.println("\ntaskName saved at index "+index+" is : "+tasksNames.get(temps));
+					System.out.println("task Description saved at index "+index+" is : "+taskDescText.get(temps));
+					System.out.println("Start date saved at index "+index+" is : "+startDates.get(temps));
+					System.out.println("End date saved at index "+index+" is : "+endDates.get(temps));
+					System.out.println("ringtones saved at index "+index+" is : "+ringtones.get(temps));
+					System.out.println();
+				}
+			}
+		});
+	}
+
+	protected static void updateValues() {
+		tasksNames.set(selected,taskNameInput.getText());
+		task.set(selected, taskNameInput.getText());
+		taskDescText.set(selected, taskDescBox.getText());
+		startDates.set(selected,startDateInput.getText());
+		endDates.set(selected, endDateInput.getText());
+		ringtones.set(selected, ringtoneInput.getText());
+	}
+
+	private static void delFunction() {
+		deleteTask.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				tasksNames.remove(selected);
+				taskDescText.remove(selected);
+				startDates.remove(selected);
+				endDates.remove(selected);
+				ringtones.remove(selected);
+				
+				task.remove(selected);
+				System.out.println("\n list item Replaced with "+tasksNames.get(selected));
+			}
+		});
+	}
+
 	private static void listFunction() {
 		
 		taskList.addMouseListener(new MouseAdapter() {
 			
 			public void mouseClicked(MouseEvent e)
 			{
-				String selected="";
-//				 if (taskList.getSelectedIndex() != -1) {                       
-//		             
-//		          } 
-				selected = taskList.getSelectedValue();   
+				selected = taskList.getSelectedIndex();
+				System.out.println("the index which is selected is "+selected);
 	             System.out.println("Selected list items is "+selected); 
 	             frame.add(leftPanel1);
 	             frame.validate();
 	             frame.repaint();
 	             setInputFields(taskList.getSelectedIndex());
-	             
+	             updateFunction();
+	             delFunction();
 			}
 			
 		});
@@ -95,10 +150,19 @@ public class Functionality extends MainFrame{
 	protected static void setInputFields(int i) {
 		// TODO Auto-generated method stub
 		taskNameInput.setText(tasksNames.get(i));
+		tempTaskName=tasksNames.get(i);
+		
 		taskDescBox.setText(taskDescText.get(i));
+		tempTaskName=tasksNames.get(i);
+		
 		startDateInput.setText(startDates.get(i));
+		tempTaskName=tasksNames.get(i);
+		
 		endDateInput.setText(endDates.get(i));
+		tempTaskName=tasksNames.get(i);
+		
 		ringtoneInput.setText(ringtones.get(i));
+		tempTaskName=tasksNames.get(i);
 	}
 
 	protected static void saveDetailsList() {
