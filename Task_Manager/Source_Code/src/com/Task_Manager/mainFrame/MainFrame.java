dbbff158cbd7d7f9fef36b7f1ccb30827f336d8c
aeloori revanth kumar <71598984+aeloori@users.noreply.github.com>
2023-08-15 16:21:08 +0530

    Gui Phase 1.3
    Changed date and time inputs from text Fields to combo boxes
    
    Work-
    -------
    * Need to modify Functionality code for the combo boxes

--- Task_Manager/Source_Code/src/com/Task_Manager/mainFrame/MainFrame.java
+++ Task_Manager/Source_Code/src/com/Task_Manager/mainFrame/MainFrame.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 //import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -20,6 +21,10 @@ import javax.swing.SwingUtilities;
 
 public class MainFrame
 {
+//	this classs object
+	static MainFrame startCal;
+	static MainFrame endCal;
+	
 //	components
 	protected static  JFrame frame;
 	
@@ -32,7 +37,6 @@ public class MainFrame
 	protected static JScrollPane taskListScroll;
 	
 	protected static JPanel leftPanel1;
-//	protected static JPanel leftPanel2;
 	
 //	task name
 	protected static JLabel taskName;
@@ -45,7 +49,12 @@ public class MainFrame
 	
 //	startdate
 	protected static JLabel startDate;
-	protected static JTextField startDateInput;
+	protected JComboBox<String> monthsList;
+	protected JComboBox<String> datesList;
+	protected JComboBox<String> yearsList;
+	
+	protected JComboBox<String> hourList;
+	protected JComboBox<String> minutesList;
 	
 //	enddate
 	protected static JLabel endDate;
@@ -61,8 +70,14 @@ public class MainFrame
 	protected static JButton deleteTask;
 	
 	
+	final protected static String[] monthsArry= {"Months","January","Febuary","March","April","May","June","July","August","September","October","November","December"};
+	final protected static String[] dateArry= {"date","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
+	final protected static String[] yearArry= {"Year","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022","2023","2024","2025","2026","2027","2028","2029","2030","2031"};
+	final protected static String[] hourArry= {"Hour","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","00"};
+	final protected static String[] minutesArray= {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60"};
 	
 	final private static Font font=new Font("Tahoma",Font.PLAIN,20);
+	final private static Font fontS=new Font("Tahoma",Font.PLAIN,14);
 	final private static Font fontM=new Font("Tahoma",Font.PLAIN,16);
 	final private static Font fontB=new Font("Tahoma",Font.BOLD,20);
 	
@@ -78,13 +93,13 @@ public class MainFrame
 					public void run() {
 						taskList.clearSelection();
 						taskList.setEnabled(false);
-						Functionality.setInputFields();
+//						Functionality.setInputFields();
 						setTask.setVisible(true);
 						setTask.setEnabled(true);
-						deleteTask.setVisible(false);
-						deleteTask.setEnabled(false);
-						updateTask.setVisible(false);
-						updateTask.setEnabled(false);
+//						deleteTask.setVisible(false);
+//						deleteTask.setEnabled(false);
+//						updateTask.setVisible(false);
+//						updateTask.setEnabled(false);
 						frame.add(leftPanel1);
 						frame.validate();
 						frame.repaint();
@@ -118,36 +133,38 @@ public class MainFrame
 		leftPanel1.add(taskDesc);
 		leftPanel1.add(taskDescBox);
 		leftPanel1.add(startDate);
-		leftPanel1.add(startDateInput);
+		
+//		calender day components for start 
+		leftPanel1.add(startCal.datesList);
+		leftPanel1.add(startCal.monthsList);
+		leftPanel1.add(startCal.yearsList);
+		
+//		time components for start
+		leftPanel1.add(startCal.hourList);
+		leftPanel1.add(startCal.minutesList);
+		
+//		calender day components for end 
+		leftPanel1.add(endCal.datesList);
+		leftPanel1.add(endCal.monthsList);
+		leftPanel1.add(endCal.yearsList);
+		
+//		time components for start
+		leftPanel1.add(endCal.hourList);
+		leftPanel1.add(endCal.minutesList);
+		
 		leftPanel1.add(endDate);
-		leftPanel1.add(endDateInput);
 		leftPanel1.add(ringtone);
 		leftPanel1.add(ringtoneInput);
 		leftPanel1.add(setTask);
 		leftPanel1.add(updateTask);
 		leftPanel1.add(deleteTask);
 		
-		//panel for existing task
-//		leftPanel2.add(taskName);
-//		leftPanel2.add(taskNameInput);
-//		leftPanel2.add(starBtn);
-//		leftPanel2.add(taskDesc);
-//		leftPanel2.add(taskDescBox);
-//		leftPanel2.add(startDate);
-//		leftPanel2.add(startDateInput);
-//		leftPanel2.add(endDate);
-//		leftPanel2.add(endDateInput);
-//		leftPanel2.add(ringtone);
-//		leftPanel2.add(ringtoneInput);
-//		leftPanel2.add(deleteTask);
-//		leftPanel2.add(updateTask);
-		
-		
 	}
 
 	final private static void initialize() {
 //		UIManager.put("Button.select", "#219EBC");
-		
+		startCal=new MainFrame();
+		endCal=new MainFrame();
 		
 //		initialize Main frame
 		frame=new JFrame("Task Manager");
@@ -199,13 +216,6 @@ public class MainFrame
 		leftPanel1.setBackground(Color.white);
 		leftPanel1.setLayout(null);
 		
-		//left panel for existing task edit
-//		leftPanel2=new JPanel();
-//		leftPanel2.setBorder(null);
-//		leftPanel2.setBounds(459, 32, 796, 628);
-//		leftPanel2.setBackground(Color.white);
-//		leftPanel2.setLayout(null);
-		
 //		task input box with label
 		
 		//label
@@ -252,14 +262,32 @@ public class MainFrame
 		startDate.setBounds(33,397, 122,24);
 		startDate.setText("Date of start");
 		startDate.setFont(font);
+				
+		//datelist for combo box
+		startCal.datesList=new JComboBox<String>(dateArry);
+		startCal.datesList.setBounds(174, 398,73, 22);
+		startCal.datesList.setFont(fontS);
 		
-		//input
-		startDateInput=new JTextField();
-		startDateInput.setBounds(215,387, 326, 44);
-		startDateInput.setColumns(16);
-		startDateInput.setBackground(Color.decode("#D9D9D9"));
-		startDateInput.setFont(fontM);
-		startDateInput.setBorder(null);
+		//monthsList 
+		startCal.monthsList=new JComboBox<String>(monthsArry);
+		startCal.monthsList.setBounds(255, 398,73, 22);
+		startCal.monthsList.setFont(fontS);
+		
+		//yearlist for combo box
+		startCal.yearsList=new JComboBox<String>(yearArry);
+		startCal.yearsList.setBounds(336, 398,73, 22);
+		startCal.yearsList.setFont(fontS);
+		
+//		time
+		//hour combo box
+		startCal.hourList=new JComboBox<String>(hourArry);
+		startCal.hourList.setBounds(417, 398,73, 22);
+		startCal.hourList.setFont(fontS);
+		
+		//minute combo box
+		startCal.minutesList=new JComboBox<String>(minutesArray);
+		startCal.minutesList.setBounds(417, 398,73, 22);
+		startCal.minutesList.setFont(fontS);
 		
 //		date of end
 		//label
@@ -268,13 +296,33 @@ public class MainFrame
 		endDate.setText("End date");
 		endDate.setFont(font);
 		
-		//input
-		endDateInput=new JTextField();
-		endDateInput.setBounds(215,467, 326, 44);
-		endDateInput.setColumns(16);
-		endDateInput.setBackground(Color.decode("#D9D9D9"));
-		endDateInput.setFont(fontM);
-		endDateInput.setBorder(null);
+//		date of end
+				
+		//datelist for combo box
+		endCal.datesList=new JComboBox<String>(dateArry);
+		endCal.datesList.setBounds(174, 478,73, 22);
+		endCal.datesList.setFont(fontS);
+		
+		//monthsList 
+		endCal.monthsList=new JComboBox<String>(monthsArry);
+		endCal.monthsList.setBounds(255, 478,73, 22);
+		endCal.monthsList.setFont(fontS);
+		
+		//yearlist for combo box
+		endCal.yearsList=new JComboBox<String>(yearArry);
+		endCal.yearsList.setBounds(336, 478,73, 22);
+		endCal.yearsList.setFont(fontS);
+		
+//		time
+		//hour combo box
+		endCal.hourList=new JComboBox<String>(hourArry);
+		endCal.hourList.setBounds(417, 478,73, 22);
+		endCal.hourList.setFont(fontS);
+		
+		//minute combo box
+		endCal.minutesList=new JComboBox<String>(minutesArray);
+		endCal.minutesList.setBounds(417, 478,73, 22);
+		endCal.minutesList.setFont(fontS);
 		
 //		Ringtone
 		//label
